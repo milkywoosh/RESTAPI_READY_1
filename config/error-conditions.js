faba732f47e3d3125b1652926de00dd050be0b8f
lukmanx <archer.lukman@gmail.com>
2022-03-13 12:02:20 +0700

    add sequelize

---
+++ config/error-conditions.js
@@ -0,0 +1,9 @@
+export const ERRORS = {
+    NOT_FOUND: "not_found",
+    EMAIL_ALREADY_EXIST: "email_already_exist",
+    BAD_REQUEST: 'bad_request',
+    EMAIL_NOT_FOUND: 'email_not_found',
+    WRONG_EMAIL_OR_PASSWORD: 'wrong_email_and_password',
+    FORBIDDEN: 'forbidden',
+    INTERNAL_SERVER_ERROR: 'internal_server_error'
+  };
